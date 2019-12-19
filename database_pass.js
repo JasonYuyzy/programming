494666449681c0bae3ae6f9df588ae338543b14a
JasonYuyzy <44361310+JasonYuyzy@users.noreply.github.com>
2019-12-19 15:02:37 +0000

    Delete database_pass.js

--- database_pass.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	"url": 'mongodb+srv://dev:DGm49ihsQr3qP%40Z@just-rwic4.gcp.mongodb.net/test?retryWrites=true&w=majority'
-	//"url": 'mongodb+srv://fish1:lqy666666@yddfish-7wnp1.mongodb.net/test?retryWrites=true&w=majority'
-};
