cd6e128013b8f5635107e03b7e47a2d8b6a58cac
YDD <zhengyu.yu2@durham.ac.uk>
2019-12-19 11:38:11 +0000

    test

---
+++ database_pass.js
@@ -0,0 +1,4 @@
+module.exports = {
+	"url": 'mongodb+srv://dev:DGm49ihsQr3qP%40Z@just-rwic4.gcp.mongodb.net/test?retryWrites=true&w=majority'
+	//"url": 'mongodb+srv://fish1:lqy666666@yddfish-7wnp1.mongodb.net/test?retryWrites=true&w=majority'
+};
