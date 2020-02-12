da406dee0c93a567ff8c5c534858ba10542a5b9f
YDD <zhengyu.yu2@durham.ac.uk>
2020-02-12 14:41:11 +0000

    add the new code for will_be_alive function

--- file/test.c
+++ file/test.c
@@ -18,10 +18,10 @@ void read_in_file (FILE *infile, struct universe *u)
     struct universe u_new;
     struct universe u_previous;
 
-    u_new.mat = (char **)malloc(sizeof(char *) * 10);
+    u_new.mat = (char **)malloc(sizeof(char *) * height(row));
     for (i = 0; i < 10; i ++)
     { //distribute every column base on each row
-    	u_new.mat[i] = (char *)malloc(sizeof(char) * 10);
+    	u_new.mat[i] = (char *)malloc(sizeof(char) * width(column));
     }
 
     u_previous.mat = (char **)malloc(sizeof(char *) * 10);
@@ -31,12 +31,18 @@ void read_in_file (FILE *infile, struct universe *u)
     }
 }
 
+void write_out_file (FILE *outfile, struct universe *u)
+{
+
+}
+
 int is_alive (struct universe *u, int column, int row)
 {
     if (u->mat[(row-1+height)%height][(column-1+width)%width] == '*')
     {
         return 1;
-    } else
+    }
+    else
     {
         return 0;
     }
@@ -45,10 +51,93 @@ int is_alive (struct universe *u, int column, int row)
 int will_be_alive (struct universe *u, int column, int row)
 {
     int alive = 0;
-    if (column == 0)
+    if (row == 0 && column == 0)
     {
-
+        //right
+        if (u->mat[row][column+1] == '*')
+        {
+            alive = alive + 1;
+        }
+        //down
+        if (u->mat[row+1][column] == '*')
+        {
+            alive = alive + 1;
+        }
+        //down right
+        if (u->mat[row+1][column+1] == '*')
+        {
+            alive = alive + 1;
+        }
     }
+    else if (row == 0)
+    {
+        //left
+        if (u->mat[row][column-1] == '*')
+        {
+            alive = alive + 1;
+        }
+        //right
+        if (u->mat[row][column+1] == '*')
+        {
+            alive = alive + 1;
+        }
+        //down left
+        if (u->mat[row+1][column-1] == '*')
+        {
+            alive = alive + 1;
+        }
+        //down
+        if (u->mat[row+1][column] == '*')
+        {
+            alive = alive + 1;
+        }
+        //down right
+        if (u->mat[row+1][column+1] == '*')
+        {
+            alive = alive + 1;
+        }
+    }
+    else if (column == 0)
+    {
+        //top middle
+        if (u->mat[row+1][column] == '*')
+        {
+            alive = alive + 1;
+        }
+        //top right
+        if (u->mat[row+1][column+1] == '*')
+        {
+            alive = alive + 1;
+        }
+        //right
+        if (u->mat[row][column+1] == '*')
+        {
+            alive = alive + 1;
+        }
+        //down
+        if (u->mat[row+1][column] == '*')
+        {
+            alive = alive + 1;
+        }
+        //down right
+        if (u->mat[row+1][column+1] == '*')
+        {
+            alive = alive + 1;
+        }
+    }
+    else if (row == height && column == width)
+    {
+        //in the down left corner
+    }
+    else if (row == height)
+    {
+        //in the bottom
+    }
+    else if (column == width)
+    {
+        //in the left most
+    }
+    //top left
     if (u->mat[(row-1+height)%height][(column-1+width)%width] == '*')
     {
         alive = alive + 1;
