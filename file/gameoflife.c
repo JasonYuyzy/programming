8cdabdf9b3753b78198d62d9f2b8a7cbc68ba137
YDD <zhengyu.yu2@durham.ac.uk>
2020-02-17 02:10:55 +0000

    prepare the command line part

--- file/gameoflife.c
+++ file/gameoflife.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include"gol.h"
-int main(int argc, char argv[]){
-  struct universe v; 
-  read_in_file(stdin,&me)
+int main(int argc, char *argv[]){
+  struct universe v;
+  read_in_file(stdin,&v);
   evolve(&v,will_be_alive);
   evolve(&v,will_be_alive);
   evolve(&v,will_be_alive);
@@ -10,4 +10,11 @@ int main(int argc, char argv[]){
   evolve(&v,will_be_alive);
   write_out_file(stdout,&v);
 return 0;
+}
+
+#include<stdio.h>
+#include"gol.h"
+int main(int argc, char *argv[])
+{
+
 }
