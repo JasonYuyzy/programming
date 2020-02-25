ab4d1e56e0bda24d1fddec06715e934eeaadfbf2
Jason Yu <zhengyu.yu2@durham.ac.uk>
2020-02-25 16:57:52 +0000

    new changes with the statistic part

--- file/gameoflife.c
+++ file/gameoflife.c
@@ -142,7 +142,7 @@ int main(int argc, char *argv[])
 
     u.alive_num = 0;
     u.alive_average = 0;
-    u.statistic = 0;
+    statistic = 0;
     u.alive_average = 0;
 
     for (int i = 0; i < u.generation_num; ++i)
@@ -150,18 +150,24 @@ int main(int argc, char *argv[])
         if (use_torus)
         {
             evolve(&u,will_be_alive_torus);
-            print_statistics (&u);
-        }
+		}
         else
         {
             evolve(&u,will_be_alive);
-            print_statistics (&u);
-        }
+		}
+		
+		if (statistic)
+		{
+			print_statistics (&u);
+		}
         //printf("%f of the cells currently alive \n", u.statistic);
-        write_out_file(outfile, &u);
+        //write_out_file(outfile, &u);
     }
-    //printf("%f of cells alive on average\n", u.alive_average);
-
+	
+	if (statistic)
+	{
+		printf("%f of cells alive on average\n", u.alive_average);
+	}
 
 
     write_out_file(outfile, &u);
