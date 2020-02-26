27776b00a9f09a68ba9f47a5c9dcb0deba8451b3
YDD <zhengyu.yu2@durham.ac.uk>
2020-02-26 22:28:36 +0000

    change of th rule and fix the bugs of the command line input

--- file/gameoflife.c
+++ file/gameoflife.c
@@ -9,7 +9,31 @@ int main(int argc, char *argv[])
 {
     struct universe u;
 
-    int opt, use_torus=0, print_stat=0;
+    int i_check=0, o_check=0, g_check=0, s_check=0, t_check=0;
+    int i, opt, use_torus=0, print_stat=0;
+    for (i = 1; i < argc; ++i)
+    {
+        if (!strcmp("-i", argv[i]))
+        {
+            i_check = 1;
+        }
+        if (!strcmp("-o", argv[i]))
+        {
+            o_check = 1;
+        }
+        if (!strcmp("-g", argv[i]))
+        {
+            g_check = 1;
+        }
+        if (!strcmp("-s", argv[i]))
+        {
+            s_check = 1;
+        }
+        if (!strcmp("-t", argv[i]))
+        {
+            t_check = 1;
+        }
+    }
 
     char *optstr = "i:o:g:st";
 	//set the number to check the command line option
@@ -19,16 +43,21 @@ int main(int argc, char *argv[])
         switch (opt)
         {
             case 'i':
-                if (!i_num)
-                {
-                    printf("'-i' repeated!!\n");
-                    return 0;
-                }
+                //if (!i_num)
+                //{
+                    //printf("'-i' repeated!!\n");
+                    //return 0;
+                //}
 
                 if(!strcmp("-s", optarg) || !strcmp("-g", optarg) || !strcmp("-o", optarg) || !strcmp("-t", optarg))
                 {
                     printf("wrong input command line: please input the correct input file name!\n");
-                    return 0;
+                    exit(0);
+                }
+                if ( strcmp(u.inputFileName, optarg) && i_num == 0)
+                {
+                    printf("The file name input is not the same\n");
+                    exit(0);
                 }
                 u.inputFileName = optarg;
                 u.inputFile = 1;
@@ -39,13 +68,13 @@ int main(int argc, char *argv[])
                 if (!o_num)
                 {
                     printf("'-o' repeated!!\n");
-                    return 0;
+                    exit(0);
                 }
 
                 if(!strcmp("-s", optarg) || !strcmp("-g", optarg) || !strcmp("-i", optarg) || !strcmp("-t", optarg))
                 {
                     printf("wrong input command line: please input the correct output file name!\n");
-                    return 0;
+                    exit(0);
                 }
                 u.outputFileName = optarg;
                 u.outputFile = 1;
@@ -56,27 +85,32 @@ int main(int argc, char *argv[])
                 if (!g_num)
                 {
                     printf("'-g' repeated!!\n");
-                    return 0;
+                    exit(0);
                 }
 				
 				if(!strcmp("-s", optarg) || !strcmp("-i", optarg) || !strcmp("-o", optarg) || !strcmp("-t", optarg))
                 {
                     printf("wrong input command line: please input the correct number of generation!\n");
-                    return 0;
+                    exit(0);
                 }
 				else if (atoi(optarg) ==0)
 				{
 					printf("wrong input command line: please input the correct number of generation!\n");
-					return 0;
+					exit(0);
 				}
-                u.generation_num = atoi(optarg);
+                u.generation_num = atof(optarg);
+                if ( atof(optarg) != (int)atof(optarg) )
+                {
+                    printf("Please input the integer for the generation number!\n");
+                    exit(0);
+                }
                 g_num = 0;
                 break;
             case 's':
                 if (!s_num)
                 {
                     printf("'-s' repeated!!\n");
-                    return 0;
+                    exit(0);
                 }
 
                 print_stat = 1;
@@ -86,14 +120,14 @@ int main(int argc, char *argv[])
                 if (!t_num)
                 {
                     printf("option '-t' repeated!!\n");
-                    return 0;
+                    exit(0);
                 }
 
                 use_torus = 1;
                 t_num = 0;
                 break;
 
-            default:
+            case '?':
                 if(strchr(optstr, optopt) == NULL)
                 {
                     fprintf(stderr, "unknown option '-%c'\n", optopt);
@@ -102,10 +136,15 @@ int main(int argc, char *argv[])
                 {
                     fprintf(stderr, "option '-%c' requires an argument \n", optopt);
                 }
-                return 0;
+                exit(0);
         }
     }
-	//check the option '-i' whether inputed
+    if (i_num == i_check || g_num == g_check || o_num == o_check || s_num == s_check || t_num == t_check)
+    {
+        printf("The error input of the command line\n");
+        exit(0);
+    }
+	//check the option '-i' whether inputted
     if (i_num)
     {
         u.inputFile = 0;
@@ -137,7 +176,7 @@ int main(int argc, char *argv[])
 
     read_in_file(infile, &u);
 
-    for (int i = 0; i < u.generation_num; ++i)
+    for ( i = 0; i < u.generation_num; ++i)
     {
         if (use_torus)
         {
